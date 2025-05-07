15c122c6fa7ad8708a6989b0231525db5706379f
aha-hyeong <ahahyeong@gmail.com>
2025-05-07 10:08:38 +0900

    회원가입 페이지, 이메일 유효성 검사 기능 추가

--- pandaMarket/script/sign.js
+++ pandaMarket/script/sign.js
@@ -4,8 +4,7 @@ function setupPasswordToggle(inputId, toggleId) {
   const input = document.getElementById(inputId);
 
   toggle.addEventListener("click", () => {
-    const type =
-      input.getAttribute("type") === "password" ? "text" : "password";
+    const type = input.getAttribute("type") === "password" ? "text" : "password";
     input.setAttribute("type", type);
     toggle.classList.toggle("fa-eye");
     toggle.classList.toggle("fa-eye-slash");
@@ -15,3 +14,28 @@ function setupPasswordToggle(inputId, toggleId) {
 setupPasswordToggle("user-password", "togglePassword");
 setupPasswordToggle("user-password-check", "togglePasswordCheck");
 // 비밀번호 눈모양 토글 끝
+
+// 회원가입 이메일 유효성 검사
+const elInputUserEmail = document.querySelector(".email-input");
+const elEmailError = document.querySelector("#user-email-error");
+const elEmailEmptyError = document.querySelector("#user-email-empty-error");
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+elInputUserEmail.addEventListener("blur", () => {
+  const emailValue = elInputUserEmail.value.trim();
+
+  if (emailValue === "") {
+    elInputUserEmail.style.outline = "2px solid #f74747";
+    elEmailError.style.display = "none";
+    elEmailEmptyError.style.display = "block";
+  } else if (!emailRegex.test(emailValue)) {
+    elInputUserEmail.style.outline = "2px solid #f74747";
+    elEmailError.style.display = "block";
+    elEmailEmptyError.style.display = "none";
+  } else {
+    elInputUserEmail.style.outline = "2px solid #f74747";
+    elEmailError.style.display = "none";
+    elEmailEmptyError.style.display = "none";
+  }
+});
